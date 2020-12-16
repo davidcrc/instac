9d5f9d6730a6113fdb0a7f8bd91ba4a535aa51f9
davidcrc <david_drcr@hotmail.com>
2020-12-16 10:41:34 -0500

    7-1. Separando zonas de auth y invitados

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React, { useState } from "react";
 import { Button } from 'semantic-ui-react'
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo";
+import Auth from "./pages/Auth";
 
 function App() {
+  // const [auth, setAuth] = useState({name: "DAvid"})  // Con un login activo
+  const [auth, setAuth] = useState(undefined)         // Significa si estado o sin loguin
+
   return (
     <ApolloProvider client={client} >
 
-      <div className="App" >
-        <h1>En App</h1>
-        <Button primary>Primary</Button>
-        <Button secondary>Secondary</Button>
-      </div>
+      {
+        !auth ? <Auth/> : <h1>Estas logueado</h1>
+      }
     </ApolloProvider>
   );
 }
