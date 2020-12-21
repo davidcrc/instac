1866687cf1c4dc3b2c4a8f2e13ef3ad2d678190f
davidcrc <david_drcr@hotmail.com>
2020-12-21 09:28:54 -0500

    8-2. Creando todas las paginas de nuestra  aplicación

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import useAuth from "../../hooks/useAuth";
+import "./Home.scss";
 
 export default function Home() {
-    const auth = useAuth();
+    // const auth = useAuth();
 
-    console.log(auth)
+    // console.log(auth)
     return (
         <div>
             <div>
