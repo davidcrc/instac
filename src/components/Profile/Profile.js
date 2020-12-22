d7143f074304f10e63da8f07936f5673a9a745ce
davidcrc <david_drcr@hotmail.com>
2020-12-22 11:03:03 -0500

    11-2. Opciones de la actualización del avatar

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 import { Grid, Image } from "semantic-ui-react";
 import { useQuery } from "@apollo/client";
 import { GET_USER } from "../../gql/user";
+import userAuth from "../../hooks/useAuth";
 import UserNotFound from "../../components/UserNotFound";
 import ModalBasic from "../Modal/ModalBasic";
+import AvatarForm from "../User/AvatarForm";
 import ImageNoFound from "../../assets/avatar.png";
 import "./Profile.scss"
 
 export default function Profile(props) {
     const {username} = props;
     const  [showModal, setShowModal ] = useState(false)
+    const [titleModal, setTitleModal] = useState("")
+    const [childrenModal, setChildrenModal] = useState(null)
+    const { auth } = userAuth();
     // const result = useQuery(GET_USER , {
     const {data, loading, error } = useQuery(GET_USER , {
         variables: { username }
@@ -22,12 +27,25 @@ export default function Profile(props) {
     
     
     console.log("getUser ",getUser)
+
+    const handlerModal = (type) => {
+        switch (type) {
+            case "avatar":
+                setTitleModal("Cambiar foto de perfil")
+                setChildrenModal(<AvatarForm setShowModal={setShowModal} />)
+                setShowModal(true)
+                break;
+        
+            default:
+                break;
+        }
+    }
     
     return (
         <>
             <Grid className="profile" >
                 <Grid.Column width={5} className="profile__left" >
-                    <Image src={ImageNoFound} avatar onClick={ () => setShowModal(true)} />
+                    <Image src={ImageNoFound} avatar onClick={ () => {if (username === auth.username) handlerModal("avatar")} } />
                 </Grid.Column>
 
                 <Grid.Column width={11} className="profile__right" >
@@ -51,10 +69,8 @@ export default function Profile(props) {
                 </Grid.Column>
             </Grid>
 
-            <ModalBasic show={showModal} setShow={setShowModal} title="Subir avatar" >
-                <p>Opciones...</p>
-                <p>Opciones...</p>
-                <p>Opciones...</p>
+            <ModalBasic show={showModal} setShow={setShowModal} title={titleModal} >
+                {childrenModal}
             </ModalBasic>
         </>
     )
