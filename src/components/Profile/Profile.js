325a7af43a429ede7b9ab1eb8b55ec8da1df454e
davidcrc <david_drcr@hotmail.com>
2020-12-22 10:02:46 -0500

    11-1. BasicModal

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, Image } from "semantic-ui-react";
 import { useQuery } from "@apollo/client";
 import { GET_USER } from "../../gql/user";
 import UserNotFound from "../../components/UserNotFound";
+import ModalBasic from "../Modal/ModalBasic";
 import ImageNoFound from "../../assets/avatar.png";
 import "./Profile.scss"
 
 export default function Profile(props) {
     const {username} = props;
+    const  [showModal, setShowModal ] = useState(false)
     // const result = useQuery(GET_USER , {
     const {data, loading, error } = useQuery(GET_USER , {
         variables: { username }
@@ -19,13 +21,13 @@ export default function Profile(props) {
     const {getUser} = data;
     
     
-    console.log(getUser)
+    console.log("getUser ",getUser)
     
     return (
         <>
             <Grid className="profile" >
                 <Grid.Column width={5} className="profile__left" >
-                    <Image src={ImageNoFound} avatar />
+                    <Image src={ImageNoFound} avatar onClick={ () => setShowModal(true)} />
                 </Grid.Column>
 
                 <Grid.Column width={11} className="profile__right" >
@@ -48,6 +50,12 @@ export default function Profile(props) {
                     </div>
                 </Grid.Column>
             </Grid>
+
+            <ModalBasic show={showModal} setShow={setShowModal} title="Subir avatar" >
+                <p>Opciones...</p>
+                <p>Opciones...</p>
+                <p>Opciones...</p>
+            </ModalBasic>
         </>
     )
 }
