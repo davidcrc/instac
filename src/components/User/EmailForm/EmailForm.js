6acb8ea34732e8f58baa2372f2a64b59d43e7842
davidcrc <david_drcr@hotmail.com>
2020-12-30 11:57:45 -0500

    12-12. Actualizando el email del usuario

--- src/components/User/EmailForm/EmailForm.js
+++ src/components/User/EmailForm/EmailForm.js
@@ -3,13 +3,14 @@ import { Form, Button } from "semantic-ui-react";
 import { useFormik } from "formik"
 import * as Yup from "yup";
 import { toast } from "react-toastify";
-// import { useMutation } from "@apollo/client";
-// import { UPDATE_USER } from "../../../gql/user";
+import { useMutation } from "@apollo/client";
+import { UPDATE_USER } from "../../../gql/user";
 import "./EmailForm.scss"
 
 export default function EmailForm(props) {
     // console.log(props)
     const {setShowModal, currentEmail} = props;
+    const [updateUser] = useMutation(UPDATE_USER)
 
     const formik = useFormik({
         initialValues: {
@@ -22,11 +23,17 @@ export default function EmailForm(props) {
           console.log(formData)
           
           try {
-
+            await updateUser({
+                variables: {
+                    input: formData
+                }
+            })
+            // console.log(result)
+            setShowModal(false)
 
         } catch (error) {
             console.log("err formEmail", error)
-            toast.error("Error al cambiar email")
+            toast.error("Error al actualizar email")
           }
         },
     });
