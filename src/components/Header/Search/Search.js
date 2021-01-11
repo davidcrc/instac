0e0419cefe6d2a1ba867b9aeaebffd02ef6cf97f
davidcrc <david_drcr@hotmail.com>
2021-01-11 11:31:13 -0500

    133. Ejecutando la query en el buscador

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Search as SearchSUi } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { SEARCH } from "../../../gql/user";
 import "./Search.scss"
 
 export default function Search() {
+
+    const [search, setSearch] = useState(null);
+    // console.log(search)
+    const {data, loading} = useQuery(SEARCH, {
+        variables: { search }
+    })
+
+    // console.log(data);
+
+    const onChange = (e) => {
+        if(e.target.value)
+            setSearch(e.target.value)
+        else
+            setSearch(null)
+    }
+
     return (
         <SearchSUi 
             className="search-users"
             fluid
             input={{ icon: "search", iconPosition: "left" }}
+            onSearchChange={ onChange }
         />
     )
 }
