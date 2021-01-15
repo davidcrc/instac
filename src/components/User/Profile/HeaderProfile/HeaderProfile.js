df2b724eef6e60bf0566b3e8baa6aab74e9a6842
davidcrc <david_drcr@hotmail.com>
2021-01-15 11:42:38 -0500

    14-5. Botón Follow o UnFollow

--- src/components/User/Profile/HeaderProfile/HeaderProfile.js
+++ src/components/User/Profile/HeaderProfile/HeaderProfile.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { Button } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { IS_FOLLOW } from "../../../../gql/follow";
 import "./HeaderProfile.scss"
 
 export default function HeaderProfile(props) {
 
     const { getUser, auth, handlerModal } = props
+    const { data, loading } = useQuery(IS_FOLLOW, {
+        variables: {
+            username: getUser.username
+        }
+    });
+    // console.log(data)
+    const buttonFollow = () => {
+        if(data.isFollow) {
+            return <Button className="btn-danger" >
+                Dejar de seguir
+            </Button>
+        }else {
+            return <Button className="btn-action" >
+                Seguir
+            </Button>
+        }
+    }
+
     return (
         <div className="header-profile" >
             <h2>{getUser.username}</h2>
@@ -12,7 +32,7 @@ export default function HeaderProfile(props) {
                 getUser.username === auth.username ? (
                     <Button onClick={() => handlerModal("settings")} >Ajustes</Button>
                 ) : (
-                    <Button>Seguir</Button>
+                    !loading && buttonFollow()
 
                 )
             }
