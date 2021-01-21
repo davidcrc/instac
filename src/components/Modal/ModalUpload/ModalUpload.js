25fbbfa28035ce02a72f8ecba22029040b615d2b
davidcrc <david_drcr@hotmail.com>
2021-01-21 11:37:21 -0500

    15-2. Añadiendo React DropZone al modal

--- src/components/Modal/ModalUpload/ModalUpload.js
+++ src/components/Modal/ModalUpload/ModalUpload.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Modal, Icon, Button, Dimmer, Loader } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
 import "./ModalUpload.scss"
 
 export default function ModalUpload(props) {
     
     const { show, setShow} = props;
+
+    const onDrop = useCallback(( acceptedFile ) => {
+        const file = acceptedFile[0];
+        console.log(file)
+    })
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/jpeg, image/png, image/jpg",
+        noKeyboard: true,
+        multiple: false,
+        onDrop
+    })
+    
 
     const onClose = () => {
         setShow(false);
@@ -12,7 +26,11 @@ export default function ModalUpload(props) {
     
     return (
         <Modal size="small" open={show} onClose={onClose}  className="modal-upload" >
-            <h1>El modal de subir</h1>
+            <div {...getRootProps()} className="drop-zone" >
+                <Icon name="cloud upload" />
+                <p>Arrastra o clickea aqui para publicar tu foto o video </p>
+                <input {...getInputProps()} />
+            </div>
         </Modal>
     )
 }
