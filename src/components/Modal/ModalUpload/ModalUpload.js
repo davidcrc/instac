7535538106704daca07d90cfb8e667ca7cce7648
davidcrc <david_drcr@hotmail.com>
2021-01-21 11:52:17 -0500

    15-3. Mostrando la imagen que se quiere publicar

--- src/components/Modal/ModalUpload/ModalUpload.js
+++ src/components/Modal/ModalUpload/ModalUpload.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Modal, Icon, Button, Dimmer, Loader } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
 import "./ModalUpload.scss"
@@ -6,10 +6,17 @@ import "./ModalUpload.scss"
 export default function ModalUpload(props) {
     
     const { show, setShow} = props;
+    const [fileUpload, setFileUpload] = useState(null)
+
+    // console.log(fileUpload)
 
     const onDrop = useCallback(( acceptedFile ) => {
         const file = acceptedFile[0];
-        console.log(file)
+        setFileUpload({
+            type: "image",
+            file,
+            preview: URL.createObjectURL(file)
+        })
     })
 
     const { getRootProps, getInputProps } = useDropzone({
@@ -22,15 +29,31 @@ export default function ModalUpload(props) {
 
     const onClose = () => {
         setShow(false);
+        // setFileUpload(null);     //opcional
     }
     
     return (
         <Modal size="small" open={show} onClose={onClose}  className="modal-upload" >
-            <div {...getRootProps()} className="drop-zone" >
-                <Icon name="cloud upload" />
-                <p>Arrastra o clickea aqui para publicar tu foto o video </p>
+            <div 
+                {...getRootProps()} 
+                className="drop-zone"  
+                style={ fileUpload &&  { border: 0}  } 
+            >
+                { !fileUpload  && (
+                    <>
+                        <Icon name="cloud upload" />
+                        <p>Arrastra o clickea aquí para publicar tu foto o video. </p>
+                    </>
+                )}
                 <input {...getInputProps()} />
             </div>
+
+            { fileUpload?.type === "image" && (
+                    <div className="image" style={{ backgroundImage: `url("${fileUpload.preview}")` }} >
+
+                    </div>
+            )}
+
         </Modal>
     )
 }
